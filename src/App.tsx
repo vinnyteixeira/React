f8d5062ddeeadf6a42bf8859c97a7039ea7387a7
Vinny Teixeira <87780451+vinnyteixeira@users.noreply.github.com>
2022-01-20 15:47:13 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
 import React from "react";
+import Navbar from "./components/estaticos/navbar/Navbar";
+import Footer from "./components/estaticos/footer/Footer";
 import Home from "./paginas/home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Login from "./paginas/login/Login";
+import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <div style={{ minHeight: "100vh" }}>
+          <Route exact path="/">
+            <Login />
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/cadastrousuario">
+            <CadastroUsuario />
+          </Route>
+        </div>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
