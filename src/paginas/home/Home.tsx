40ddda1546f842b494762e58d4bab0cc5c2777fe
Vinny Teixeira <87780451+vinnyteixeira@users.noreply.github.com>
2022-01-24 09:09:34 -0300

    Projeto blog-pessoal generation

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,15 +1,60 @@
 import React from "react";
+import { Typography, Box, Grid, Button } from "@material-ui/core";
+import TabPostagem from "../../components/postagens/tabpostagem/TabPostagem";
 import "./Home.css";
 
 function Home() {
   return (
     <>
-      <h1 className="titulo">Home</h1>
-      <img
-        src="https://cdn.pixabay.com/photo/2015/05/26/23/52/technology-785742_960_720.jpg"
-        alt="Imagem tela inicial"
-        className="img"
-      />
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        className="caixa"
+      >
+        <Grid alignItems="center" item xs={6}>
+          <Box paddingX={20}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              component="h3"
+              align="center"
+              className="titulo"
+            >
+              Seja bem vindo(a)!
+            </Typography>
+            <Typography
+              variant="h5"
+              gutterBottom
+              color="textPrimary"
+              component="h5"
+              align="center"
+              className="titulo"
+            >
+              expresse aqui os seus pensamentos e opiniões!
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="center">
+            <Box marginRight={1}></Box>
+            <Button variant="outlined" className="botao">
+              Ver Postagens
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src="https://i.imgur.com/H88yIo2.png"
+            alt=""
+            width="500px"
+            height="500px"
+          />
+        </Grid>
+        <Grid xs={12} className="postagens">
+          <TabPostagem />
+        </Grid>
+      </Grid>
     </>
   );
 }
